8433599559a0b653e23abc17109392c083c40b1b
Julio David <isaacdvid15.96@outlook.com>
2022-06-06 14:35:36 -0600

    added session cookie

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
-
+const session = require('express-session')
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
@@ -24,9 +24,25 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60  * 60 * 24 * 7,
+        maxAge: 1000 * 60  * 60 * 24 * 7 
+    }
+}
+
+app.use(session(sessionConfig))
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
+
+
 
 app.get('/', (req,res) => {
     res.render('home')
