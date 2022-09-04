74df97a6d039fd3e1a979757c7b8e08608f1f200
Julio David <isaacdvid15.96@outlook.com>
2022-09-03 20:35:33 -0600

    styled register, login and home. sanitize mongo

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const userRoutes = require('./routes/users')
+const mongoSanitize = require('express-mongo-sanitize')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser:true, useUnifiedTopology:true})
 
@@ -35,6 +36,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret: 'secret',
